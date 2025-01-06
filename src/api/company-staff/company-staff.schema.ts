31cf37d4760820b3291a3f2974e582b294113c8f
Enoch Klufio <Enochmk94@gmail.com>
2025-01-06 22:55:17 +0000

    feat: enhance company staff schema with validation for new fields and update request types

--- src/api/company-staff/company-staff.schema.ts
+++ src/api/company-staff/company-staff.schema.ts
@@ -1,24 +1,38 @@
 import { RequestHandler } from 'express';
 import z from 'zod';
+import { MAX_PASSWORD_LENGTH, MIN_PASSWORD_LENGTH } from '../../utils/constants';
 
 export const addCompanyStaffSchema = z.object({
   body: z.object({
+    firstName: z.string().min(2, 'First name is too short').max(100, 'First name is too long'),
+    lastName: z.string().min(2, 'Last name is too short').max(100, 'Last name is too long'),
+    email: z.string().email(),
+    phoneNumber: z.string(),
+    password: z
+      .string()
+      .min(MIN_PASSWORD_LENGTH, 'Password is too short')
+      .max(MAX_PASSWORD_LENGTH, 'Password is too long'),
+    roleId: z.string().uuid().optional(),
+  }),
+  params: z.object({
     companyId: z.string().uuid(),
-    userId: z.string().uuid(),
   }),
 });
-
 export type AddCompanyStaffInput = z.infer<typeof addCompanyStaffSchema>['body'];
 
-export type AddCompanyStaffRequest = RequestHandler<any, any, AddCompanyStaffInput>;
+export type AddCompanyStaffRequest = RequestHandler<
+  z.infer<typeof addCompanyStaffSchema>['params'],
+  any,
+  z.infer<typeof addCompanyStaffSchema>['body']
+>;
 
 export const removeCompanyStaffSchema = z.object({
-  body: z.object({
+  params: z.object({
     companyId: z.string().uuid(),
     userId: z.string().uuid(),
   }),
 });
 
-export type RemoveCompanyStaffInput = z.infer<typeof removeCompanyStaffSchema>['body'];
+export type RemoveCompanyStaffInput = z.infer<typeof removeCompanyStaffSchema>['params'];
 
-export type RemoveCompanyStaffRequest = RequestHandler<any, any, RemoveCompanyStaffInput>;
+export type RemoveCompanyStaffRequest = RequestHandler<RemoveCompanyStaffInput, any, any>;
