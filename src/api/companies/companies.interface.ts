d09f64748e0e5961d70b6c44be25f4cfe5b89410
Enoch Klufio <Enochmk94@gmail.com>
2025-01-06 19:20:31 +0000

    feat: add company selection interface and integrate it into company model methods

---
+++ src/api/companies/companies.interface.ts
@@ -0,0 +1,8 @@
+import { Prisma } from '@prisma/client';
+
+export const companySelect: Prisma.companySelect = {
+  id: true,
+  name: true,
+  createdAt: true,
+  updatedAt: true,
+};
--- src/api/companies/companies.model.ts
+++ src/api/companies/companies.model.ts
@@ -1,9 +1,11 @@
 import prisma from '../../db/prisma.db';
+import { companySelect } from './companies.interface';
 import { CreateCompanyInput, UpdateCompanyInput } from './companies.schema';
 
 export const createCompany = async (data: CreateCompanyInput) => {
   return prisma.company.create({
     data,
+    select: companySelect,
   });
 };
 
@@ -11,6 +13,7 @@ export const updateCompany = async (companyId: string, data: UpdateCompanyInput)
   return prisma.company.update({
     where: { id: companyId },
     data,
+    select: companySelect,
   });
 };
 
@@ -23,6 +26,7 @@ export const deleteCompany = async (companyId: string) => {
 export const findCompanyById = async (companyId: string) => {
   return prisma.company.findUnique({
     where: { id: companyId },
+    select: companySelect,
   });
 };
 
