c1ba893d040ab45d3bdd346d0006249839daef98
Enoch Klufio <Enochmk94@gmail.com>
2025-01-06 17:00:06 +0000

    refactor: enhance logging in company middleware and service for improved traceability

--- src/api/companies/companies.middleware.ts
+++ src/api/companies/companies.middleware.ts
@@ -1,15 +1,20 @@
 import { RequestHandler } from 'express';
 import * as service from './companies.service';
 import createHttpError from 'http-errors';
+import { getLogger } from '../../utils/logger';
+
+const logger = getLogger('CompanyMiddleware');
 
 export const checkCompanyExists: RequestHandler = async (req, res, next) => {
   const companyIds = [req.params?.companyId, req.body?.companyId].filter(Boolean);
+  logger.verbose('Checking if company exists ... ', { companyIds });
   if (companyIds.length === 0) return next();
   for (const companyId of companyIds) {
+    logger.verbose(`Checking company with id :${companyId}... `);
     const company = await service.getCompanyById(companyId);
     if (!company) {
       return next(new createHttpError.NotFound(`Company ${companyId} not found`));
     }
   }
-  next();
+  return next();
 };
--- src/api/companies/companies.service.ts
+++ src/api/companies/companies.service.ts
@@ -1,23 +1,40 @@
+import { getLogger } from '../../utils/logger';
 import * as model from './companies.model';
 import { CreateCompanyInput, UpdateCompanyInput } from './companies.schema';
 
+const logger = getLogger('CompanyService');
+
 export const getCompanies = async () => {
-  return model.getCompanies();
+  logger.verbose('Fetching companies ...');
+  const response = await model.getCompanies();
+  logger.info('Companies fetched successfully', response);
+  return response;
 };
 
 export const getCompanyById = async (companyId: string) => {
+  logger.verbose(`Fetching company with id ${companyId} ...`);
   const company = await model.findCompanyById(companyId);
+  logger.info('Company fetched successfully', company);
   return company;
 };
 
 export const createCompany = async (data: CreateCompanyInput) => {
-  return model.createCompany(data);
+  logger.verbose('Creating company ...', data);
+  const response = await model.createCompany(data);
+  logger.info('Company created successfully', response);
+  return response;
 };
 
 export const updateCompany = async (companyId: string, data: UpdateCompanyInput) => {
-  return model.updateCompany(companyId, data);
+  logger.verbose(`Updating company with id ${companyId} ...`, data);
+  const response = await model.updateCompany(companyId, data);
+  logger.info('Company updated successfully', response);
+  return response;
 };
 
 export const deleteCompany = async (companyId: string) => {
-  return model.deleteCompany(companyId);
+  logger.verbose(`Deleting company with id ${companyId} ...`);
+  const response = await model.deleteCompany(companyId);
+  logger.info('Company deleted successfully', response);
+  return response;
 };
