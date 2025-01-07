3d129eca9304044f6642ec4bfe13b84190b69a85
Enoch Klufio <Enochmk94@gmail.com>
2025-01-07 00:42:45 +0000

    feat: remove company staff controller, middleware, model, service, and schema for codebase cleanup

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -6,7 +6,6 @@ import userRoutes from './users/users.routes';
 import roleRoutes from './roles/roles.routes';
 import companyRoutes from './companies/companies.routes';
 import companyAdminRoutes from './company-admins/company-admins.routes';
-import companyStaffRoutes from './company-staff/company-staff.routes';
 import * as authMiddleware from './auth/auth.middleware';
 
 const router = Router();
@@ -22,7 +21,6 @@ router.use(authMiddleware.validateCurrentUser);
 router.use('/users', userRoutes);
 router.use('/roles', roleRoutes);
 router.use('/companies/:companyId', companyAdminRoutes);
-router.use('/companies/:companyId', companyStaffRoutes);
 router.use('/companies', companyRoutes);
 
 export default router;
