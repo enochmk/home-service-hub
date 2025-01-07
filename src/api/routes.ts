77381ff50acc3323858c3fcfae5aea7f6a5a607c
Enoch Klufio <Enochmk94@gmail.com>
2025-01-07 00:43:49 +0000

    feat: remove company admin routes for codebase simplification

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -5,7 +5,6 @@ import authRoutes from './auth/auth.routes';
 import userRoutes from './users/users.routes';
 import roleRoutes from './roles/roles.routes';
 import companyRoutes from './companies/companies.routes';
-import companyAdminRoutes from './company-admins/company-admins.routes';
 import * as authMiddleware from './auth/auth.middleware';
 
 const router = Router();
@@ -20,7 +19,6 @@ router.use(authMiddleware.verifyJWT);
 router.use(authMiddleware.validateCurrentUser);
 router.use('/users', userRoutes);
 router.use('/roles', roleRoutes);
-router.use('/companies/:companyId', companyAdminRoutes);
 router.use('/companies', companyRoutes);
 
 export default router;
