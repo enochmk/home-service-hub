fac6b3c3c128673f870b6932ee29982daeae843c
Enoch Klufio <Enochmk94@gmail.com>
2025-01-26 14:41:04 +0000

    chore: comment out company routes in API router

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -4,7 +4,7 @@ import { Router } from 'express';
 import authRoutes from './auth/auth.routes';
 import userRoutes from './users/users.routes';
 import roleRoutes from './roles/roles.routes';
-import companyRoutes from './companies/companies.routes';
+// import companyRoutes from './companies/companies.routes';
 // import companyAdminsRoutes from './company-admins/company-admins.routes';
 import * as authMiddleware from './auth/auth.middleware';
 import * as companyAdminMiddleware from './company-admins/company-admins.middleware';
@@ -24,6 +24,6 @@ router.use(authMiddleware.checkUserCompanyAssociation);
 router.use('/users', userRoutes);
 router.use('/roles', roleRoutes);
 // router.use('/companies/:companyId', companyAdminsRoutes);
-router.use('/companies', companyRoutes);
+// router.use('/companies', companyRoutes);
 
 export default router;
