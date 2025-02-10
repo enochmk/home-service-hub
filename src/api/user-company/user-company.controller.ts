4724685a416c4b5b58abf66bb33cad32dd64fa99
Enoch Klufio <Enochmk94@gmail.com>
2025-02-10 18:43:04 +0000

    refactor: migrate user-company logic from service to model and enhance logging

--- src/api/user-company/user-company.controller.ts
+++ src/api/user-company/user-company.controller.ts
@@ -1,26 +1,38 @@
 import { RequestHandler } from 'express';
-import * as service from './user-company.service';
+import * as model from './user-company.model';
 import { AddCompanyUserRequest, RemoveCompanyUserRequest } from './user-company.schema';
+import { getLogger } from '../../utils/logger';
+
+const logger = getLogger('UserCompanyController');
 
 export const addUserToCompany: AddCompanyUserRequest = async (req, res) => {
-  const response = await service.addUserToCompany(req.params.companyId, req.body.userId);
+  logger.verbose('Adding user to company');
+  const response = await model.addUserToCompany(req.params.companyId, req.body.userId);
+  logger.info('User added to company', { response });
   res.status(201).json(response);
 };
 
 export const removeUserFromCompany: RemoveCompanyUserRequest = async (req, res) => {
   const { companyId, userId } = req.params;
-  await service.removeUserFromCompany(companyId, userId);
+  logger.verbose('Removing user from company', { companyId, userId });
+  await model.removeUserFromCompany(companyId, userId);
+  logger.info('User removed from company', { companyId, userId });
   res.status(204).send();
 };
 
 export const getCompanyUsers: RequestHandler = async (req, res) => {
   const { companyId } = req.params;
-  const response = await service.getCompanyUsers(companyId);
+  logger.verbose('Fetching users by companyId', { companyId });
+  const data = await model.findCompanyUsers(companyId);
+  const response = data.map((item) => item.user);
+  logger.info('Company Users fetched', response);
   res.status(200).json(response);
 };
 
 export const getCompanyUserByUserId: RequestHandler = async (req, res) => {
   const { companyId, userId } = req.params;
-  const response = await service.getCompanyUserById(companyId, userId);
+  logger.verbose('Fetching user by userId', { companyId, userId });
+  const response = await model.findCompanyUserByCompanyIdAndUserId(companyId, userId);
+  logger.info('Company user fetched', response);
   res.status(200).json(response);
 };
