a31d792e9d4db87aac7c5e5d1ecc37cf5ca0dc5d
Enoch Klufio <Enochmk94@gmail.com>
2025-03-20 14:34:24 +0000

    feat: enhance task types controller with typed request handlers for improved type safety

--- src/api/task-types/task-types.controller.ts
+++ src/api/task-types/task-types.controller.ts
@@ -1,6 +1,8 @@
 import { RequestHandler } from 'express';
 import { getLogger } from '../../utils/logger';
 import prisma from '../../db/prisma.db';
+import { CreateTaskTypeInput } from './task-types.schema';
+import { UpdateRequestInput } from '../requests/requests.schema';
 
 const logger = getLogger('TaskTypes');
 
@@ -11,7 +13,8 @@ export const getAllTaskTypes: RequestHandler = async (req, res) => {
   res.status(200).json({ data });
 };
 
-export const getTaskTypeById: RequestHandler = async (req, res) => {
+type GetRequest = RequestHandler<{ taskTypeId: string }>;
+export const getTaskTypeById: GetRequest = async (req, res) => {
   const taskTypeId = parseInt(req.params.taskTypeId);
   logger.verbose('Getting task type by id', { taskTypeId });
   const data = await prisma.taskTypes.findUnique({ where: { id: taskTypeId } });
@@ -19,7 +22,8 @@ export const getTaskTypeById: RequestHandler = async (req, res) => {
   res.status(200).json(data);
 };
 
-export const createTaskType: RequestHandler = async (req, res) => {
+type CreateRequest = RequestHandler<unknown, unknown, CreateTaskTypeInput>;
+export const createTaskType: CreateRequest = async (req, res) => {
   const { name } = req.body;
   logger.verbose('Creating task type', { name });
   const data = await prisma.taskTypes.create({ data: { name } });
@@ -27,7 +31,8 @@ export const createTaskType: RequestHandler = async (req, res) => {
   res.status(201).json(data);
 };
 
-export const updateTaskType: RequestHandler = async (req, res) => {
+type UpdateRequest = RequestHandler<{ taskTypeId: string }, any, UpdateRequestInput>;
+export const updateTaskType: UpdateRequest = async (req, res) => {
   const taskTypeId = parseInt(req.params.taskTypeId);
   logger.verbose('Updating task type', { taskTypeId, ...req.body });
   const { name } = req.body;
@@ -39,7 +44,8 @@ export const updateTaskType: RequestHandler = async (req, res) => {
   res.status(200).json(data);
 };
 
-export const deleteTaskType: RequestHandler = async (req, res) => {
+type DeleteRequest = RequestHandler<{ taskTypeId: string }>;
+export const deleteTaskType: DeleteRequest = async (req, res) => {
   const taskTypeId = parseInt(req.params.taskTypeId);
   logger.verbose('Deleting task type', { taskTypeId });
   await prisma.taskTypes.delete({ where: { id: taskTypeId } });
