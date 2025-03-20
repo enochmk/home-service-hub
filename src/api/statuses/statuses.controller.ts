112c799f381845fb72ddb69c5deaccbee663b8c3
Enoch Klufio <Enochmk94@gmail.com>
2025-03-20 13:56:57 +0000

    feat: enhance statuses API with logging and validation checks

--- src/api/statuses/statuses.controller.ts
+++ src/api/statuses/statuses.controller.ts
@@ -1,26 +1,45 @@
 import { RequestHandler } from 'express';
+import { getLogger } from '../../utils/logger';
+import prisma from '../../db/prisma.db';
+
+const logger = getLogger('StatusesController');
 
 export const getAllStatuses: RequestHandler = async (req, res) => {
-  res.status(200).json({ roles: null });
+  logger.verbose('Fetching all statuses');
+  const data = await prisma.statuses.findMany();
+  logger.info('Fetched all statuses', data);
+  res.status(200).json({ data: data });
 };
 
 export const getStatusById: RequestHandler = async (req, res) => {
   const statusId = parseInt(req.params.statusId);
-  res.status(200).json({ statusId });
+  logger.verbose('Fetching status by ID', { statusId });
+  const data = await prisma.statuses.findUnique({ where: { id: statusId } });
+  logger.info('Fetched status by ID', data);
+  res.status(200).json(data);
 };
 
 export const createStatus: RequestHandler = async (req, res) => {
   const { name } = req.body;
-  res.status(201).json({ name });
+  logger.verbose('Creating new status', { name });
+  const data = await prisma.statuses.create({ data: { name } });
+  logger.info('Created new status', data);
+  res.status(201).json(data);
 };
 
 export const updateStatus: RequestHandler = async (req, res) => {
   const statusId = parseInt(req.params.statusId);
   const { name } = req.body;
-  res.status(200).json({ statusId, name });
+  logger.verbose('Updating status', { statusId, name });
+  const data = await prisma.statuses.update({ where: { id: statusId }, data: { name } });
+  logger.info('Updated status', data);
+  res.status(200).json(data);
 };
 
 export const deleteStatus: RequestHandler = async (req, res) => {
-  // const statusId = parseInt(req.params.statusId);
+  const statusId = parseInt(req.params.statusId);
+  logger.verbose(`Deleting status with ID: ${statusId}`);
+  await prisma.statuses.delete({ where: { id: statusId } });
+  logger.info('Deleted status successfully', { statusId });
   res.sendStatus(204);
 };
--- src/api/statuses/statuses.middleware.ts
+++ src/api/statuses/statuses.middleware.ts
@@ -1,9 +1,41 @@
 import { NextFunction, Request, Response } from 'express';
+import createHttpError from 'http-errors';
+import { getLogger } from '../../utils/logger';
+import prisma from '../../db/prisma.db';
+
+const logger = getLogger('StatusesMiddleware');
 
 export async function checkStatusIdExist(req: Request, _res: Response, next: NextFunction) {
+  const targetId = parseInt(req.params.statusId, 10);
+  logger.verbose('Checking status ID exist', { statusId: targetId });
+  const status = await prisma.statuses.findMany({
+    where: {
+      id: targetId,
+    },
+  });
+
+  // ! If the status is empty, throw a 404 error
+  if (status.length === 0) {
+    throw createHttpError.NotFound(`Status: ${targetId} not found`);
+  }
+
+  logger.info('Status ID exist', { status });
   return next();
 }
 
 export async function checkStatusNameAvailable(req: Request, _res: Response, next: NextFunction) {
+  const { name } = req.body;
+  logger.verbose('Checking status name available', { name });
+  const status = await prisma.statuses.findMany({
+    where: {
+      name,
+    },
+  });
+
+  if (status.length > 0) {
+    throw createHttpError.Conflict(`This name: ${name} already exists`);
+  }
+
+  logger.info('Status name available for use', { name });
   return next();
 }
